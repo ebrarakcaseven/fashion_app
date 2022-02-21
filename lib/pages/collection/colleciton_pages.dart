963af7d5ddf9ccafccce7ff76ff352be0ab0c05f
ebrar <92300256+ebrarakcaseven@users.noreply.github.com>
2022-02-21 10:20:20 +0300

    collection sayfası eklendi

--- lib/pages/collection/colleciton_pages.dart
+++ lib/pages/collection/colleciton_pages.dart
@@ -99,7 +99,8 @@ class _CollectionState extends State<Collection> {
                                 ),
                                 Image(image: NetworkImage(mypost['image'])),
                                 Padding(
-                                  padding: const EdgeInsets.only(top: 20.0),
+                                  padding: const EdgeInsets.only(
+                                      top: 20.0, bottom: 5.0),
                                   child: Text("${mypost['title']}",
                                       style: const TextStyle(
                                           fontSize: 15, color: Colors.white)),
@@ -112,109 +113,119 @@ class _CollectionState extends State<Collection> {
                     });
               }
             }),
-        Padding(
-          padding: const EdgeInsets.only(right: 90.0, left: 90.0, top: 50.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const Icon(FontAwesomeIcons.twitter, color: Colors.white),
-              const Icon(FontAwesomeIcons.instagram, color: Colors.white),
-              const Icon(FontAwesomeIcons.youtube, color: Colors.white),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              StreamBuilder<QuerySnapshot>(
-                  stream: _statusService.getContact(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Text(
-                        'no data...',
-                      );
-                    } else {
-                      return ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: snapshot.data?.docs.length,
-                          itemBuilder: (context, index) {
-                            DocumentSnapshot mypost =
-                                snapshot.data!.docs[index];
-                            // ignore: avoid_unnecessary_containers
-                            return Container(
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text("${mypost['email']}",
-                                        style: const TextStyle(
-                                            fontSize: 16, color: Colors.white)),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text("${mypost['phone']}",
-                                        style: const TextStyle(
-                                            fontSize: 16, color: Colors.white)),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text("${mypost['workingHours']}",
-                                        style: const TextStyle(
-                                            fontSize: 16, color: Colors.white)),
-                                  ),
-                                ],
-                              ),
-                            );
-                          });
-                    }
-                  }),
-            ],
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            Padding(
-              // ignore: prefer_const_constructors
-              padding: EdgeInsets.only(left: 50.0, top: 50.0, bottom: 20.0),
-              child: InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const About()),
-                ),
-                child: const Text("About",
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
-              child: Text("Contact",
-                  style: TextStyle(color: Colors.white, fontSize: 18)),
-            ),
+        Container(
+          height: 350,
+          color: Colors.white,
+          child: Column(children: [
             Padding(
               padding:
-                  const EdgeInsets.only(right: 50.0, top: 50.0, bottom: 20.0),
-              child: InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Blog()),
-                ),
-                // ignore: avoid_unnecessary_containers
-                child: Container(
-                  child: const Text("Blog",
-                      style: TextStyle(color: Colors.white, fontSize: 18)),
-                ),
+                  const EdgeInsets.only(right: 90.0, left: 90.0, top: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Icon(FontAwesomeIcons.twitter, color: Colors.black),
+                  const Icon(FontAwesomeIcons.instagram, color: Colors.black),
+                  const Icon(FontAwesomeIcons.youtube, color: Colors.black),
+                ],
               ),
             ),
-          ],
-        )
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  StreamBuilder<QuerySnapshot>(
+                      stream: _statusService.getContact(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Text(
+                            'no data...',
+                          );
+                        } else {
+                          return ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              itemCount: snapshot.data?.docs.length,
+                              itemBuilder: (context, index) {
+                                DocumentSnapshot mypost =
+                                    snapshot.data!.docs[index];
+                                // ignore: avoid_unnecessary_containers
+                                return Container(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text("${mypost['email']}",
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.black)),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text("${mypost['phone']}",
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.black)),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text("${mypost['workingHours']}",
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.black)),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                        }
+                      }),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  // ignore: prefer_const_constructors
+                  padding: EdgeInsets.only(left: 50.0, top: 50.0, bottom: 20.0),
+                  child: InkWell(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const About()),
+                    ),
+                    child: const Text("About",
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
+                  child: Text("Contact",
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      right: 50.0, top: 50.0, bottom: 20.0),
+                  child: InkWell(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Blog()),
+                    ),
+                    // ignore: avoid_unnecessary_containers
+                    child: Container(
+                      child: const Text("Blog",
+                          style: TextStyle(color: Colors.black, fontSize: 18)),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ),
       ])),
     );
   }
